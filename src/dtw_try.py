97dc6c04c8ec1e92d48f7aef11313b515a44f5bc
Dimitrios Kirtsios <dimkirts@gmail.com>
2016-11-24 21:23:34 +0200

    Demo of pitch extraction and DTW matching
    Trying to match the original .pv file of the dataset with the one
    created from the extractor
    
    pitch extraction done using zero crossing rate
    matching done using fast DTW

---
+++ src/dtw_try.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy.spatial.distance import euclidean
+
+from fastdtw import fastdtw
+
+pitch_vector_1 = []
+pitch_vector_2 = []
+
+f1 = open("data/00013.pv", "r")
+for line in f1:
+  pitch_vector_1.append(float(line.rstrip()))
+
+f2 = open("data/zeropv.pv", "r")
+for line in f2:
+  pitch_vector_2.append(float(line.rstrip()))
+
+#print pitch vectors
+print "Pitch Vector 1"
+print pitch_vector_1
+
+print "Pitch Vector 2"
+print pitch_vector_2
+
+distance, path = fastdtw(pitch_vector_1, pitch_vector_2, dist=euclidean)
+print "Distance is : " + str(distance)
+
+f1.close()
+f2.close()
