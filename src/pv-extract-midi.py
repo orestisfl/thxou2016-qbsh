5685e4dc6e77cdfe117aa42047eec61e88d9a7c9
iodim <ioanndima@gmail.com>
2016-11-25 22:17:04 +0200

    pv-extract-midi now accepts files from command line

--- src/pv-extract-midi.py
+++ src/pv-extract-midi.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 
-import music21
-import math
 import os
-
-path = 'MIR-QBSH-corpus/midiFile/'
+import sys
+import math
+import music21
 
 framesize = 0.01
 
-for file in os.listdir(path):
+for file in sys.argv[1:]:
     filename, fileext = os.path.splitext(file)
     if fileext == '.mid':
         print('Processing ' + filename + '.mid', end = '\r')
-        md = music21.converter.parse(path + filename + '.mid').flat
+        md = music21.converter.parse(filename + '.mid').flat
 
         p = [note.pitch.midi for note in md.notes]
         reps = [math.floor(note['durationSeconds']/framesize) for note in md.notes.secondsMap]
@@ -20,7 +19,7 @@ for file in os.listdir(path):
         # Make pv and flatten it
         pv = [item for sublist in [[e] * r for e,r in zip(p, reps)] for item in sublist]
 
-        f = open(path + filename + '.pv', 'w')
+        f = open(filename + '.pv', 'w')
         for p in pv:
             f.write("%s\n" % p)
         f.close()
