703a04c447aa513419ccd5f1d3c72aea0ff44c2e
iodim <ioanndima@gmail.com>
2016-11-26 04:10:36 +0200

    Formatting

--- src/pitch_extraction_try.py
+++ src/pitch_extraction_try.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Audio file
 # Kai mp3 pairnei kai wav
-filename = "data/00013.wav"
+filename = 'data/00013.wav'
 
 # 2. Load the audio as a waveform `y`
 #    Store the sampling rate as `sr`
@@ -13,7 +13,7 @@ filename = "data/00013.wav"
 y, sr = librosa.load(filename, sr=None ,offset=0.0)
 
 # Create destination file
-f = open("data/zeropv.pv", "w")
+f = open('data/zeropv.pv', 'w')
 
 
 # Using zero crossing rate, frame size 256
@@ -22,6 +22,6 @@ for counter in range(0, len(y)/256):
   #print(z)
   z1 = np.vstack([y[256*counter:counter*256+256], z]).T
   z2 = np.nonzero(z)
-  f.write(str(len(z2[0]))+"\n")
+  f.write(str(len(z2[0])) + '\n')
 
 f.close()
