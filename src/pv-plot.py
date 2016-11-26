ed1345664699c263cd41489ab0036c2c760a7b53
iodim <ioanndima@gmail.com>
2016-11-26 02:37:45 +0200

    Added script to plot and compare PVs

---
+++ src/pv-plot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+import numpy as np
+from fastdtw import fastdtw
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import euclidean
+
+midi = sys.argv[1]
+wave = sys.argv[2]
+midipv = np.array([float(line.strip()) for line in open(midi, 'r')])
+wavepv = np.array([float(line.strip()) for line in open(wave, 'r')])
+
+idx = np.arange(0, len(wavepv))
+
+plt.plot(idx, wavepv, idx, midipv[idx])
+plt.show()
