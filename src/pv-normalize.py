62124dc2261b6a355cffefafc648421877c65ccb
iodim <ioanndima@gmail.com>
2016-11-26 04:10:36 +0200

    Normalization now removes all zero-pitch values

--- src/pv-normalize.py
+++ src/pv-normalize.py
@@ -9,6 +9,8 @@ for file in sys.argv[1:]:
     if fileext == '.pv':
         print('Processing ' + filename + '.pv', end = '\r')
         pv = np.array([float(line.strip()) for line in open(file, 'r')])
+        # pv = np.trim_zeros(pv)
+        pv = pv[pv != 0]
         avg = np.mean(pv)
         std = np.std(pv)
         npv = (pv - avg)/std
