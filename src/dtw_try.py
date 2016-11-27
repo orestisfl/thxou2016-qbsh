b92361ed6e27e2b59d041ab9f328e91273360381
iodim <ioanndima@gmail.com>
2016-11-27 18:33:46 +0200

    Formatting changes

--- src/dtw_try.py
+++ src/dtw_try.py
@@ -8,23 +8,23 @@ from fastdtw import fastdtw
 pitch_vector_1 = []
 pitch_vector_2 = []
 
-f1 = open("data/00013.pv", "r")
+f1 = open('data/00013.pv', 'r')
 for line in f1:
-  pitch_vector_1.append(float(line.rstrip()))
+    pitch_vector_1.append(float(line.rstrip()))
 
-f2 = open("data/zeropv.pv", "r")
+f2 = open('data/zeropv.pv', 'r')
 for line in f2:
-  pitch_vector_2.append(float(line.rstrip()))
+    pitch_vector_2.append(float(line.rstrip()))
 
 #print pitch vectors
-print "Pitch Vector 1"
-print pitch_vector_1
+print('Pitch Vector 1')
+print(pitch_vector_1)
 
-print "Pitch Vector 2"
-print pitch_vector_2
+print('Pitch Vector 2')
+print(pitch_vector_2)
 
-distance, path = fastdtw(pitch_vector_1, pitch_vector_2, dist=euclidean)
-print "Distance is : " + str(distance)
+distance, path = fastdtw(pitch_vector_1, pitch_vector_2, dist = euclidean)
+print('Distance is: ', distance)
 
 f1.close()
 f2.close()
--- src/volume.py
+++ src/volume.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Audio file
-filename = "data/00013.wav"
+filename = 'data/00013.wav'
 
 # Volume of signal
 y, sr = librosa.load(filename, sr=8000 ,offset=0.0)
@@ -16,18 +16,18 @@ signal_volume_db = []
 signal_volume_abs = []
 
 for frameCounter in xrange(0, len(y)/256):
-  block = signal[256 * frameCounter : 256 * frameCounter + 256]
-  block_detrend = block.subtract(block.median())
-  block_volume_abs = block_detrend.abs().sum()
-  block_volume_db = 10 * np.log10(block_detrend.rmul(block_detrend).sum())
-  signal_volume_abs.append(block_volume_abs)
-  signal_volume_db.append(block_volume_db)
+    block = signal[256 * frameCounter : 256 * frameCounter + 256]
+    block_detrend = block.subtract(block.median())
+    block_volume_abs = block_detrend.abs().sum()
+    block_volume_db = 10 * np.log10(block_detrend.rmul(block_detrend).sum())
+    signal_volume_abs.append(block_volume_abs)
+    signal_volume_db.append(block_volume_db)
 
 signal_volume_abs = pd.Series(signal_volume_abs)
-signal_volume_abs.plot(use_index=True, title="Volume with Absolute")
+signal_volume_abs.plot(use_index = True, title = 'Volume with Absolute')
 plt.show()
 
 signal_volume_db = pd.Series(signal_volume_db)
-signal_volume_db.plot(use_index=True, title="Volume in db")
+signal_volume_db.plot(use_index = True, title = 'Volume in db')
 
 plt.show()
