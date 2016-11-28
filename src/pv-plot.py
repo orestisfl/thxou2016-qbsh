8513dafcc798b08ace2f907763c2faa9c6706dcb
iodim <ioanndima@gmail.com>
2016-11-28 19:41:34 +0200

    Modified pv-plot for better plots

--- src/pv-plot.py
+++ src/pv-plot.py
@@ -6,17 +6,25 @@ from fastdtw import fastdtw
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import euclidean
 
-midi = sys.argv[1]
-wave = sys.argv[2]
-midipv = np.array([float(line.strip()) for line in open(midi, 'r')])
-wavepv = np.array([float(line.strip()) for line in open(wave, 'r')])
+file_1 = sys.argv[1]
+file_2 = sys.argv[2]
+pv_1 = np.array([float(line.strip()) for line in open(file_1, 'r')])
+pv_2 = np.array([float(line.strip()) for line in open(file_2, 'r')])
 
-idx = np.arange(0, len(wavepv))
+idx = range(min(len(pv_1), len(pv_2)))
 
-distance, path = fastdtw(midipv, wavepv, dist = euclidean)
+distance, path = fastdtw(pv_1, pv_2, dist = euclidean)
 
-print('DTW distance: ', distance)
-
-plt.plot(idx, wavepv, idx, midipv[idx])
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(idx, pv_1[idx], label = file_1)
+ax.plot(idx, pv_2[idx], label = file_2)
+ax.set_ylabel('Pitch value')
+ax.set_xlabel('Frame index')
+ax.set_title('DTW Distance: ' + str(round(distance, 2)))
+ax.legend(bbox_to_anchor=(0, 1.2, 1, 0), mode='expand',
+    fontsize = 'small', borderaxespad = 0)
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width, box.height * .9])
 plt.show()
 
