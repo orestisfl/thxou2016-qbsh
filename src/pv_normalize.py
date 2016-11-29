40dbd2adc14a9f2493a2fa5705b88601c1c85f53
orestisf <orestisf1993@gmail.com>
2016-11-29 04:50:21 +0200

    Reformat pv_normalize.py in functions

--- src/pv_normalize.py
+++ src/pv_normalize.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
-
-import os
-import sys
 import numpy as np
 
-for file in sys.argv[1:]:
-    filename, fileext = os.path.splitext(file)
-    if fileext == '.pv':
-        print('Processing ' + filename + '.pv', end='\r')
-        pv = np.array([float(line.strip()) for line in open(file, 'r')])
-        # pv = np.trim_zeros(pv)
-        pv = pv[pv != 0]
-        avg = np.mean(pv)
-        std = np.std(pv)
-        npv = (pv - avg) / std
-        fd = open(filename + '.npv', 'w')
-        for p in npv:
-            fd.write("%s\n" % p)
-        fd.close()
-print()
+
+def normalize(pitch_vector):
+    pitch_vector = pitch_vector[pitch_vector != 0]
+    avg = np.mean(pitch_vector)
+    std = np.std(pitch_vector)
+    return (pitch_vector - avg) / std
+
+
+def load_pitch_vector(filename):
+    with open(filename) as file_object:
+        return np.array([float(line.strip()) for line in file_object])
+
+
+def main():
+    file_list = sys.argv[1:]
+    for filename in tqdm(file_list):
+        base_name, fileext = os.path.splitext(filename)
+        if fileext in ('.pv', '.mpv'):
+            pv = normalize(load_pitch_vector(filename))
+
+            # TODO use function from pv_extract_wave.py
+            fd = open(base_name + '.npv', 'w')
+            for p in pv:
+                fd.write("%s\n" % p)
+            fd.close()
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+    from tqdm import tqdm
+    sys.exit(main())
