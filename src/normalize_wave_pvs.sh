f3f842dd45c2a39239fd5baffc680eb9e8d137b8
iodim <ioanndima@gmail.com>
2016-12-31 01:08:15 +0200

    removed a commented out section from normalize_wave_pvs.sh

--- src/normalize_wave_pvs.sh
+++ src/normalize_wave_pvs.sh
@@ -4,8 +4,5 @@ set -e
 # Normalize pickle DBs
 # Assuming that this script is run from /src folder.
 
-# Create normalized PVs pickle db from midi files
-# find datasets/MIR-QBSH-corpus/midiFile -iname "*.pv" -print0 | xargs -0 ./pitch_vectors.py normalize --pickle --pickle-database "./midi-normalized.pickle"
-
 # Create normalized PVs pickle db from wave files
 find datasets/MIR-QBSH-corpus/waveFile -iname "*.pv" -print0 | xargs -0 ./pitch_vectors.py normalize --pickle --pickle-database "./wave-normalized.pickle"
