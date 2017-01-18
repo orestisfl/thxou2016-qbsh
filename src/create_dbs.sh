d23e54393fc5d7af1e4454e571f1fbfbe23d8457
iodim <ioanndima@gmail.com>
2017-01-19 00:01:19 +0200

    now we dump pickles in append mode, split pickle bug should be fixed

--- src/create_dbs.sh
+++ src/create_dbs.sh
@@ -6,6 +6,10 @@ set -e
 # This script will use every *.wav and *.mid file it can find recursively in this directory.
 # This is supposed to be ran once, as non-normalized PV extraction is a standard procedure, also requires a lot of time
 
+# Delete previous pickle files
+# (Now necessary as we save in append mode)
+find . -iname "*.pickle" -print0| xargs -0 rm -f
+
 # Delete previous pitch vector files.
 # Note: xargs -0 seems to be faster than find -exec since there are a lot of files.
 find . \( -iname "*.pv" -o -iname "*.mpv" -o -iname "*.npv" \) -print0 | xargs -0 rm -f
