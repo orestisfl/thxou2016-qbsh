ffc6d4fa42303d87cb135e20a195582de5dc2995
Dimitrios Kirtsios <dimkirts@gmail.com>
2017-01-18 19:11:49 +0200

    Remove unecessary files

--- src/normalize_wave_pvs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-# Normalize pickle DBs
-# Assuming that this script is run from /src folder.
-
-# Create normalized PVs pickle db from wave files
-find datasets/MIR-QBSH-corpus/waveFile -iname "*.pv" -print0 | xargs -0 ./pitch_vectors.py preprocess --pickle --pickle-database "./wave-normalized.pickle"
